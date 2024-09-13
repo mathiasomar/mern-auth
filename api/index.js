b86ff1a73dc8ea54009fd1834bbc75c2f13ef259
mathiasomar <ommykmathy@gmail.com>
2024-09-13 15:48:48 +0300

    create test api routes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 const connectDB = require('./db')
+const userRoutes = require('./routes/userRoute')
 
 connectDB(process.env.MONGO_URI)
 
 const app = express()
 const port = 3000
+
+// MIDDLEWARE
+app.use(cors())
+app.use(express.json())
+
+// ROUTES
+app.use('/api/user', userRoutes)
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Server running on port ${port}!`))
