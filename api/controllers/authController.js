709dac0b31fbdca6bb62deca4e262d03a2138f3f
mathiasomar <ommykmathy@gmail.com>
2024-09-14 11:58:42 +0300

    add a middleware and a function to handle error

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -1,15 +1,25 @@
 const User = require("../models/userModel")
 const bcrypt = require('bcryptjs')
+const { appError } = require("../utils/error")
 
-exports.signup = async (req, res) => {
-    const { username, email, password } = req.body
-    const hashedPassword = bcrypt.hashSync(password, 10)
-    const newUser = new User({ username, email, password: hashedPassword })
+exports.signup = async (req, res, next) => {
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+    // const newUser = new User({ username, email, password: hashedPassword })
 
     try {
-        await newUser.save()
+        // await newUser.save()
+        const checkUser = await User.findOne({ username: req.body.username })
+        if (checkUser) {
+            return next(appError(400, "Username already exists"))
+        }
+
+        const checkEmail = await User.findOne({ email: req.body.email })
+        if (checkEmail) {
+            return next(appError(400, "Email already exists"))
+        }
+        const user = await User.create({ ...req.body, password: hashedPassword })
         res.status(201).json({ message: "User created successfully" })
     } catch (error) {
-        res.status(500).json(error.message)
+        next(error)
     }
 }
--- api/index.js
+++ api/index.js
@@ -18,5 +18,15 @@ app.use(express.json())
 app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500
+    const message = err.message || 'Internal Server Error'
+    return res.status(statusCode).json({
+        success: false,
+        message,
+        statusCode
+    })
+})
+
+
 app.listen(port, () => console.log(`Server running on port ${port}!`))
